6b7884b1a232596cfbe4862eb094927738aa9b84
hamstar0 <webutek@gmail.com>
2021-07-01 15:28:33 -0700

    v1.2.0 - Added EntityExtensions

---
+++ ModLibsGeneral/Libraries/Entities/EntityExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+
+
+namespace ModLibsGeneral.Libraries.Entities {
+	/// <summary>
+	/// Assorted static library functions pertaining to `Entity`s (parent class of Item, NPC, Player, and Projectile), as
+	/// extensions.
+	/// </summary>
+	public static class EntityExtensions {
+		/// <summary></summary>
+		/// <param name="ent"></param>
+		/// <returns></returns>
+		public static Rectangle GetRectangle( this Entity ent ) {
+			return new Rectangle( (int)ent.position.X, (int)ent.position.Y, ent.width, ent.height );
+		}
+	}
+}
