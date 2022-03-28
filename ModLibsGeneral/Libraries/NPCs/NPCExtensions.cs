d7159ad3703536818a91d4ef8638bb1b8c7673e0
hamstar0 <webutek@gmail.com>
2022-03-28 09:34:06 -0700

    v1.6.0 - Added NPCExtensions and ProjectileExtensions

---
+++ ModLibsGeneral/Libraries/NPCs/NPCExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Terraria;
+
+
+namespace ModLibsGeneral.Libraries.NPCs {
+	/// <summary>
+	/// Assorted helpful item extension methods.
+	/// </summary>
+	public static class NPCExtensions {
+		/// <summary>
+		/// Self explanatory.
+		/// </summary>
+		/// <param name="self"></param>
+		/// <param name="npcType"></param>
+		/// <returns></returns>
+		public static bool Is( this NPC self, int npcType = -1 ) {
+			if( npcType == -1 ) {
+				return self.active;
+			} else {
+				return self.active && self.netID == npcType;
+			}
+		}
+	}
+}
