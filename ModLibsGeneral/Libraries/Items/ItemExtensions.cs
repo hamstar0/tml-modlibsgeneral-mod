f6bc1f7878c927d5d4c33d7716db7819cd265ba1
hamstar0 <webutek@gmail.com>
2022-03-28 09:12:52 -0700

    v1.6.0 - Added ItemExtensions

---
+++ ModLibsGeneral/Libraries/Items/ItemExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Terraria;
+
+
+namespace ModLibsGeneral.Libraries.Items {
+	/// <summary>
+	/// Assorted helpful item extension methods.
+	/// </summary>
+	public static class ItemExtensions {
+		/// <summary>
+		/// Self explanatory.
+		/// </summary>
+		/// <param name="self"></param>
+		/// <param name="itemType"></param>
+		/// <param name="minStack"></param>
+		/// <param name="maxStack"></param>
+		/// <returns></returns>
+		public static bool Is( this Item self, int itemType = -1, int minStack = 1, int maxStack = int.MaxValue ) {
+			if( itemType == -1 ) {
+				return self.active && self.stack >= minStack && self.stack <= maxStack;
+			} else {
+				return self.active && self.stack >= minStack && self.stack <= maxStack && self.type == itemType;
+			}
+		}
+	}
+}
